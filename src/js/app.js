211b9986d66aa9aebe4d5e9f0cc7f31e6ca1bb9d
Magnus Werner Ottesen <magnuswernerottesen@gmail.com>
2024-11-23 18:49:55 +0100

    create main styling and fetching elements from html in js

---
+++ src/js/app.js
@@ -0,0 +1,14 @@
+// Fetching button elements from HTML
+const navbarButton = document.querySelector(".navbar__button--navbar");
+const carouselButton = document.querySelector(".navbar__button--carousel");
+const cardButton = document.querySelector(".navbar__button--card");
+const buttonButton = document.querySelector(".navbar__button--button");
+const formButton = document.querySelector(".navbar__button--form");
+const mediaButton = document.querySelector(".navbar__button--media-controller");
+const searchButton = document.querySelector(".navbar__button--search-input");
+const alertButton = document.querySelector(".navbar__button--alert");
+
+// Fetching and appending the showcase section
+const main = document.querySelector("main");
+const showcaseSection = document.querySelector(".component__showcase");
+main.append(showcaseSection);
