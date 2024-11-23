5f05a119b3a371058ef735fdf7b3f91ec007f65b
Magnus Werner Ottesen <magnuswernerottesen@gmail.com>
2024-11-23 19:32:31 +0100

    create function to make a navbar on user interaction

--- src/js/app.js
+++ src/js/app.js
@@ -12,3 +12,50 @@ const alertButton = document.querySelector(".navbar__button--alert");
 const main = document.querySelector("main");
 const showcaseSection = document.querySelector(".component__showcase");
 main.append(showcaseSection);
+
+// Creating functions
+const createNavbar = () => {
+  showcaseSection.textContent = "";
+  const createdNavbar = document.createElement("nav");
+  createdNavbar.classList.add("component__1");
+  showcaseSection.appendChild(createdNavbar);
+
+  const navbarHeader = document.createElement("h2");
+  navbarHeader.textContent = "This is a navbar.";
+  const navbarParagraph1 = document.createElement("p");
+  const createdLink1 = document.createElement("a");
+  createdLink1.textContent = "here";
+  createdLink1.setAttribute("href", "./newPage.html");
+  navbarParagraph1.textContent = `It is used to navigate to different pages or to different places on the same page. Click here to go to a new page`;
+
+  const navbarParagraph2 = document.createElement("p");
+  const createdLink2 = document.createElement("a");
+  //   createdLink2.setAttribute("href");
+  createdLink2.textContent = "here";
+  navbarParagraph2.textContent = "Click here to go to a different page";
+
+  createdNavbar.append(navbarHeader, navbarParagraph1, navbarParagraph2);
+};
+
+const createCarousel = () => {};
+
+const createCard = () => {};
+
+const createButton = () => {};
+
+const createForm = () => {};
+
+const createMediaController = () => {};
+
+const createSearchField = () => {};
+
+const createAlert = (e) => {
+  showcaseSection.textContent = "";
+  alert("This is an alert");
+};
+
+// Attaching event listeners
+
+navbarButton.addEventListener("click", createNavbar);
+
+alertButton.addEventListener("click", createAlert);
