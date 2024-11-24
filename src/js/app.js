31c9db0e1569c6b6f29e2bfb1232fb3f3f91585e
Magnus Werner Ottesen <magnuswernerottesen@gmail.com>
2024-11-24 18:23:32 +0100

    created image slider

--- src/js/app.js
+++ src/js/app.js
@@ -26,17 +26,23 @@ const createNavbar = () => {
   navbarHeader.textContent = "This is a navbar.";
   const navbarParagraph1 = document.createElement("p");
   const createdLink1 = document.createElement("a");
-  createdLink1.textContent = "here";
+  createdLink1.textContent = "link";
   createdLink1.setAttribute("href", "./newPage.html");
   navbarParagraph1.textContent = `Click here to go to a new page`;
 
   const navbarParagraph2 = document.createElement("p");
   const createdLink2 = document.createElement("a");
   // createdLink2.setAttribute("href");
-  createdLink2.textContent = "here";
+  createdLink2.textContent = "link";
   navbarParagraph2.textContent = "Click here to go to a different page";
 
-  createdNavbar.append(navbarHeader, navbarParagraph1, navbarParagraph2);
+  createdNavbar.append(
+    navbarHeader,
+    navbarParagraph1,
+    navbarParagraph2,
+    createdLink1,
+    createdLink2
+  );
 };
 
 // Creating a carousel
@@ -46,7 +52,7 @@ const createCarousel = () => {
   const nextButton = document.createElement("button");
   nextButton.classList.add("carousel-button");
   nextButton.classList.add("carousel-button--next");
-  nextButton.setAttribute("data", "next");
+  nextButton.dataset.carouselButton = "next";
   nextButton.textContent = "Next";
 
   const prevButton = document.createElement("button");
@@ -54,15 +60,53 @@ const createCarousel = () => {
   prevButton.classList.add("carousel-button--prev");
   prevButton.textContent = "Previous";
 
+  const imageContainer = document.createElement("div");
+
+  const img1 = document.createElement("img");
+  img1.setAttribute(
+    "src",
+    "./assets/images/pexels-m-venter-792254-1659438.jpg"
+  );
+  // img1.setAttribute("data", "active");
+  img1.dataset.active = true;
+
+  img1.classList.add("carousel-image");
+
+  const img2 = document.createElement("img");
+  img2.setAttribute("src", "./assets/images/pexels-tobiasbjorkli-1559821.jpg");
+  img2.classList.add("carousel-image");
+
+  const img3 = document.createElement("img");
+  img3.setAttribute(
+    "src",
+    "./assets/images/pexels-tomas-malik-793526-2581925.jpg"
+  );
+  img3.classList.add("carousel-image");
+
+  imageContainer.append(img1, img2, img3);
+
   const carousel = document.createElement("div");
   carousel.classList.add("carousel");
   showcaseSection.appendChild(carousel);
-  carousel.append(prevButton, nextButton);
+  carousel.append(imageContainer, prevButton, nextButton);
 
   const carouselButtons = [nextButton, prevButton];
   carouselButtons.forEach((button) => {
     button.addEventListener("click", () => {
-      const offset = button.dataset === "next" ? +1 : -1;
+      const offset = button.dataset.carouselButton === "next" ? 1 : -1;
+      const slides = [img1, img2, img3];
+
+      const activeSlide = document.querySelector("[data-active]");
+      let newIndex = slides.indexOf(activeSlide) + offset;
+
+      if (newIndex < 0) {
+        newIndex = slides.length - 1;
+      }
+      if (newIndex >= slides.length) {
+        newIndex = 0;
+      }
+      slides[newIndex].dataset.active = true;
+      delete activeSlide.dataset.active;
     });
   });
 };
